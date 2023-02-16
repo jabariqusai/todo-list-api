e1b216452bc57dd0f9ceb7630142c3d1554a50ab
Mariam <201011@ppu.edu.ps>
2023-02-16 12:13:15 +0200

    create get endpoint

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.debug('Works');
+import { express } from 'express';
+
+const app = express();
+app.use(express.json());
+
+
+const list = [];
+
+app.get('/list', (req, res) => {
+  
+  console.log('GET /list');
+  const listItems = res.body || undefined;
+  if(!listItems){
+    res.status(404).end();
+    return;
+  }
+  
+  res.status(200).end();
+});
+
+
+
+const port = 3001;
+app.listen(port, () => console.log('listening on port ', port));
