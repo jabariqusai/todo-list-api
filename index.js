31d17d86684a53dc6970f2a6f910af4ab4a5231b
Mariam <201011@ppu.edu.ps>
2023-02-16 12:28:08 +0200

    create the delete endpoint

--- index.js
+++ index.js
@@ -7,18 +7,36 @@ app.use(express.json());
 const list = [];
 
 app.get('/list', (req, res) => {
-  
+
   console.log('GET /list');
   const listItems = res.body || undefined;
-  if(!listItems){
+  if (!listItems) {
     res.status(404).end();
     return;
   }
-  
+
   res.status(200).end();
 });
 
+app.delete('/list/:id', (req, res) => {
+  console.log('DELETE /list/:id');
+  const id = res.params.id;
 
+  const index = list.findIndex(item => item.id === id);
+  console.log("the index = ", index);
+  if (index === -1) {
+    res.status(404).send("The resource that you want to delete not found ! ");
+    return;
+  }
+
+  console.log("list before delete :", list);
+
+  list.splice(index, 1);
+
+  console.log("list after delete :", list);
+
+  res.status(204).end();
+});
 
 const port = 3001;
 app.listen(port, () => console.log('listening on port ', port));
