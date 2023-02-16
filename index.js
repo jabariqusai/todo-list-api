1ce3c8c015eaafbc8ba5250b9b6ab001ef46b5d5
NadeenJubeh <191080@ppu.edu.ps>
2023-02-16 13:54:57 +0200

    do api function

--- index.js
+++ index.js
@@ -1,42 +1,85 @@
-import { express } from 'express';
+const express = require('express');
+var cors = require('cors')
 
 const app = express();
+app.use(cors())
+
 app.use(express.json());
 
+let lists = [{
+  id:"1",
+  description:"do homework",
+  status:"DONE",
+}];
+app.post('/list', (req, res) => {
+  console.log('POST /list');
 
-const list = [];
+  const contentType = req.headers['content-type'];
 
-app.get('/list', (req, res) => {
-
-  console.log('GET /list');
-  const listItems = res.body || undefined;
-  if (!listItems) {
-    res.status(404).end();
+  if (contentType !== 'application/json') {
+    res.status(400).send("Invalid request payload");
     return;
   }
 
-  res.status(200).end();
-});
-
-app.delete('/list/:id', (req, res) => {
-  console.log('DELETE /list/:id');
-  const id = res.params.id;
-
-  const index = list.findIndex(item => item.id === id);
-  console.log("the index = ", index);
-  if (index === -1) {
-    res.status(404).send("The resource that you want to delete not found ! ");
+  const list = req.body;
+  console.log(list);
+  if (!list ||
+    !list.id ||
+    !list.description ||
+    !list.status) {
+    res.status(400).send("Invalid request payload. Expected id, description and status ");
     return;
   }
-
-  console.log("list before delete :", list);
-
-  list.splice(index, 1);
-
-  console.log("list after delete :", list);
-
-  res.status(204).end();
+  if (lists.find(item => item.id === list.id)) {
+    res.status(409).send("Resource with the same id already exists");
+    return;
+  }
+  lists.push(list);
+  res.status(201).end();
 });
 
-const port = 3001;
-app.listen(port, () => console.log('listening on port ', port));
+app.delete('/list/delete/:id', (req, res) => {
+  console.debug('DELETE /list/:id');
+  const id = req.params.id; 
+   let found = false;
+
+  lists = lists.filter(list => {
+    if (list.id === id && list.status != "DONE") {
+      found = true;
+      return false;
+    } else {      
+      return true;
+    }
+  });
+  res.status(found ? 204 : 404).end();
+
+});
+app.put('/list/:id', (req, res) => {
+  console.debug("update");
+  const id = req.params.id;
+  const list = lists.find(list => list.id === id);
+  console.debug(list);
+  if (!list) {
+    res.status(404).send("check the id number ");
+  }
+  const body = req.body;
+  console.log(body);
+  if (!body && body.status=="DONE") {
+    res.status(400);
+    return;
+  }
+  if (!body.description && !body.status) {
+    res.status(400);
+  }
+  list.description = body.description || list.description;
+  list.status = body.status || list.status;
+  res.send(list);
+
+});
+
+app.get('/',(req,res)=>{
+  res.status(200).send(lists);
+})
+
+const port = 3003;
+app.listen(port, () => console.log("Server run at port", port));
