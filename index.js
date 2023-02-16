1508d81ea814adae30e8a15808a0729d4f085014
sajedaswad <you@example.com>
2023-02-16 13:53:57 +0200

    first back end

--- index.js
+++ index.js
@@ -1,1 +1,121 @@
-console.debug('Works');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const Lists = [
+  { "id": '111', "description": 'good', "status": 'pending' },
+  { "id": '222', "description": 'good', "status": 'done' },
+  { "id": '333', "description": 'good', "status": 'done' },
+  { "id": '444', "description": 'good', "status": 'pending' },
+];
+
+app.get('/list', (req, res) => {
+  console.log('working');
+  res.status(200).send(Lists);
+});
+
+app.post('/list', (req, res) => {
+  const newItem = req.body;
+  const contentType = req.headers['content-type'];
+  if (contentType !== 'application/json') {
+    res.status(400).send('bad request, expecting json data');
+    return;
+  }
+
+
+  if (Lists.find(item => item.id === newItem.id)) {
+    res.status(409).send("go back");
+    return;
+  }
+  Lists.push(newItem);
+  res.end();
+
+});
+
+
+app.put('/list/:id', (req, res) => {
+  const id = req.params.id;
+
+  const newItem = Lists.find(item => item.id === id);
+
+  if (!newItem) {
+    res.status(404).send('the resorce does not exist ');
+  }
+  const body = req.body;
+
+  if (!body.description && !body.status) {
+    res.status(400).send("the description and status  is not found ");
+    return;
+  }
+  newItem.description= body.description || newItem.description;
+  newItem.status = body.status || newItem.status;
+  res.send(newItem);
+
+  return;
+
+});
+
+app.delete('/list/:id', (req, res) => {
+  const id = req.params.id;
+  const index = Lists.findIndex(item => item.id === id);
+  if (index === -1) {
+    res.status(404).end();
+    return;
+  }
+  Lists.splice(index, 1);
+
+  res.status(204).end();
+
+
+});
+
+
+app.listen(3002, () => {
+  console.log('hello world');
+});
+
+
+
+
+// //pagenation
+// app.get('/car', (req, res) => {
+//   const page = req.query.page ? Number(req.query.page) : 0;
+//   const size = req.query.size ? Number(req.query.size) : 4;
+//   const engineSize = req.query.engineSize ? Number(req.query.engineSize) : undefined;
+//   const color = req.query.color ? (req.query.color) : undefined;
+//   const sort = req.query.sort ? (req.query.sort) : undefined;
+//   const from = page * size;
+//   const to = from + size;
+
+
+
+//   let filteredCars = cars;
+//   if (engineSize) {
+//     filteredCars = filteredCars.filter(item => item.engineSize === engineSize);
+
+//   }
+//   if (color) {
+//     filteredCars = filteredCars.filter(item => item.color === color);
+//   }
+//   //sort number decinding 
+//   if (sort === 'engineSize') {
+//     filteredCars = [...cars].sort((a, b) => b.engineSize - a.engineSize);
+//     console.log(sort);
+
+//   }
+//   //string ascinding 
+//   if (sort === 'color') {
+//     filteredCars = [...cars].sort((a, b) =>
+//       a.color.toLowerCase() > b.color.toLowerCase() ? 1 : -1,
+//       console.log(sort)
+//     );
+//   }
+//   const total = filteredCars.length;
+
+//   res.send({
+//     total,
+//     result: filteredCars.slice(from, to),
+//   });
+// });
+
+
