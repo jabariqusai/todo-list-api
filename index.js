2bcc298d929ba6d00de27c4b992ff9ca942b600d
Mariam <201011@ppu.edu.ps>
2023-02-16 12:38:36 +0200

    create put endpoint

--- index.js
+++ index.js
@@ -38,5 +38,35 @@ app.delete('/list/:id', (req, res) => {
   res.status(204).end();
 });
 
+app.put('/list/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const itemList = list.find(item => item.id === id);
+
+  if (!itemList) {
+    //404 : not found
+    res.status(404).send("The resource that you want to update isn\'t found ! ");
+    return;
+  }
+  if (!body) {
+
+    // 400: bad request
+    res.status(400).send("Please send a body to complete the updating !");
+    return;
+  }
+  if (!body.description && !body.status) {
+
+    // 400: bad request
+    res.status(400).send("expected description or status !").end();
+    return;
+  }
+  itemList.description = body.description || itemList.description;
+  itemList.status = body.status || itemList.status;
+
+  //200: OK
+  res.status(200).send(itemList);
+});
+
+
 const port = 3001;
 app.listen(port, () => console.log('listening on port ', port));
