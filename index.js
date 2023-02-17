bfc55928acd59e1db6eea1bdbefc3aebbddb94c0
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-02-18 00:34:58 +0200

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express' ;
+import cors from 'cors' ;
 const app =  express() ;
 app.use (express.json());
+app.use (cors())
 
 const todos = [
   {id : '1' , description : "study hard" , status : 'done'} ,
