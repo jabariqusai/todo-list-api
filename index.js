b44d643323916563f4bfad86c6cb527a36926373
MoAyAd Anjjar <94171304+MoAyAd-Anjjar@users.noreply.github.com>
2023-02-19 13:46:51 +0200

    backend EndPoint

--- index.js
+++ index.js
@@ -2,43 +2,67 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //for all  data change to json
 const port = 3033;
 const todo = [];
+
+
 app.post('/', (req, res) => {
-  console.debug("List", todo);
+
+  if (req.headers['content-type'] !== 'application/json') {
+    res.status(400).send("Invalid content-type");
+    return;
+  }
   const body = req.body;
-  todo.push(body);
-  res.end();
+  if (todo.find(item => item.id === body.id)) {
+    res.status(409).send('A resource with the provided id already exists. Please call PUT / instead');
+    return;
+  }
+  else {
+    console.debug(body);
+    todo.push(body);
+    res.status(201).end();
+  }
+
 });
 
 
 app.get('/', (req, res) => {
-  console.debug("List", todo);
-  res.json(todo);
+  setTimeout(() => res.send(todo), 1000);
 });
 
 app.delete("/:id", (req, res) => {
   const id = req.params.id;
-  const found = todo.find(item => item.id === id);
-  if (found)
-    todo.filter(item => item.id !== id);
-    
-    else 
-    {
-      res.status(404);
-    }
+  const index = todo.findIndex(item => item.id === id);
 
-});
-app.put("/:id",(req,res)=>{
-  const id=req.params.id
-  const body=req.body
-  const found = todo.find(item => item.id === id);
-  if(found){
-   
+  if (index > -1) {
+
+    todo.splice(index, 1);
+    res.send(todo).end();
+  }
+  else {
+    res.status(404).send("Invalid request. item may not Exist or Deleted").status(404);
   }
 
-})
+});
+app.put("/:id", (req, res) => {
+  if (req.headers["content-type"] !== "application/json") {
+    res.status(404).send("Invalid request");
+    return
+  }
+  const id = req.params.id;
+  const body = req.body;
+
+  const index = todo.findIndex(item => item.id === id);
+  if (index === -1) {
+    res.status(404).send("Invalid request");
+  }
+
+  todo[index] = { ...body, id };
+
+  res.end();
+});
+
 
 
 app.listen(port, () => {
