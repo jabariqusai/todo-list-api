451586c5ecd2946c0e9409160e7ce5722f265a27
yara-sarahna <yarasa2211@gmail.com>
2023-02-19 11:55:01 +0200

    fix problems

--- index.js
+++ index.js
@@ -1,82 +1,69 @@
-import express from 'express';
-import cors from 'cors';
-import util from './util.js';
-
+const express = require('express');
+var cors = require('cors')
 const app = express();
-
-const items = [];
-
-app.use(cors());
+app.use(cors())
 app.use(express.json());
-
-app.post('/', (req, res) => {
-  if (req.headers['content-type'] !== 'application/json') {
-    res.status(400).send('Invalid content type');
+let lists = [{
+  id:"1",
+  description:"do homework",
+  status:"DONE",
+}];
+app.post('/list', (req, res) => {
+  console.log('POST /list');
+  const contentType = req.headers['content-type'];
+  if (contentType !== 'application/json') {
+    res.status(400).send("Invalid request payload");
     return;
   }
-
-  const body = req.body;
-  const valid = util.validateItem(body);
-
-  if (!valid) {
-    res.status(400).send('Invalid request payload');
+  const list = req.body;
+  console.log(list);
+  if (!list ||
+    !list.id ||
+    !list.description ||
+    !list.status) {
+    res.status(400).send("Invalid request payload. Expected id, description and status ");
     return;
   }
-
-  if (items.find(item => item.id === body.id)) {
-    res.status(409).send('A resource with the provided id already exists. Please call PUT / instead');
+  if (lists.find(item => item.id === list.id)) {
+    res.status(409).send("Resource with the same id already exists");
     return;
   }
-
-  items.unshift(body);
-
+  lists.push(list);
   res.status(201).end();
 });
-
-app.put('/:id', (req, res) => {
-  if (req.headers['content-type'] !== 'application/json') {
-    res.status(400).send('Invalid content type');
-    return;
-  }
-
-  const id = req.params.id;
-  const body = req.body;
-  const valid = util.validateItem(body);
-
-  if (!valid) {
-    res.status(400).send('Invalid request payload');
-    return;
-  }
-
-  const index = items.findIndex(item => item.id === id);
-
-  if (index === -1) {
-    res.status(404).send('The item you\'re trying to update does not exist. Call POST / instead');
-    return;
-  }
-
-  items[index] = { ...body, id };
-
-  res.end();
-});
-
 app.delete('/:id', (req, res) => {
   const id = req.params.id;
-
-  const index = items.findIndex(item => item.id === id);
-
+  const index = lists.findIndex(item => item.id === id);
   if (index === -1) {
     res.status(404).end();
     return;
   }
-
-  items.splice(index, 1);
-
+  lists.splice(index, 1);
   res.end();
 });
-
-app.get('/', (req, res) => {
-  setTimeout(() => res.send(items), 1000);
+app.put('/list/:id', (req, res) => {
+  console.debug("update");
+  const id = req.params.id;
+  const list = lists.find(list => list.id === id);
+  console.debug(list);
+  if (!list) {
+    res.status(404).send("check the id number ");
+  }
+  const body = req.body;
+  console.log(body);
+  if (!body && body.status=="DONE") {
+    res.status(400);
+    return;
+  }
+  if (!body.description && !body.status) {
+    res.status(400);
+  }
+  list.description = body.description || list.description;
+  list.status = body.status || list.status;
+  res.send(list);
 });
-
-app.listen(3000, () => console.debug('API is running and listening at localhost:3000'));
+app.get('/',(req,res)=>{
+  res.status(200).send(lists);
+})
+const port = 3003;
+app.listen(port, () => console.log("Server run at port", port));
